c32539d264f0dd5d2eca6ba6e3ec444d2e7fe588
Carlo <carlouxljlj@gmail.com>
2023-10-13 19:58:17 +0300

    [1] Sign In and Sign Up component creation

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './SignIn.css'
+
+function SignIn() {
+  return (
+    <>
+        <header>
+            <section className='logo'>
+                Logo
+            </section>
+        </header>
+        <body>
+            <section className='signInBox'>
+                Box
+            </section>
+        </body>
+        <footer>legend</footer>
+    </>
+  )
+}
+
+export default SignIn
