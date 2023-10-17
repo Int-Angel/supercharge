e37102cc2cd203d7eafa5f339e8b1d47274f158d
Int-Angel <padilla.angel88@gmail.com>
2023-10-17 16:20:28 +0300

    calendar style

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,31 +1,31 @@
-import React, {useState, useMemo, useCallback, useEffect} from "react";
-import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
-import {EventInteractionArgs} from 'react-big-calendar/lib/addons/dragAndDrop.js'
-import events from './events.js'
+import React, { useState, useMemo, useCallback, useEffect } from "react";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import { EventInteractionArgs } from "react-big-calendar/lib/addons/dragAndDrop.js";
+import events from "./events.js";
 
-import moment from 'moment'
+import moment from "moment";
 import "./style.scss";
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useGetTodoListsFromUserWithTodos } from "../../hooks/todo/useGetTodoListsFromUserWithTodos";
 import { useAuth } from "../../contexts/AuthProvider";
 
 // ../../hooks/todo/useGetTodoListsFromUserWithTodos.js"
 
-const DnDCalendar = withDragAndDrop(Calendar)
+const DnDCalendar = withDragAndDrop(Calendar);
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
 type TTodo = {
-  id: string,
+  id: string;
   description: string;
   list_position: number;
   completed: boolean;
   priority: number;
   start_time: Date;
   end_time: Date;
-}
+};
 
 type TCalendarEvent = {
   isDraggable: boolean;
@@ -35,16 +35,15 @@ type TCalendarEvent = {
   start: Date;
   end: Date;
   desc?: undefined;
-}
+};
 
 export default function CalendarMod() {
-
-  const [draggedEvent, setDraggedEvent] = useState<any | null>()
-  const [displayDragItemInCell, setDisplayDragItemInCell] = useState(true)
-  const [myEvents, setMyEvents] = useState<Array<TCalendarEvent>>([])
+  const [draggedEvent, setDraggedEvent] = useState<any | null>();
+  const [displayDragItemInCell, setDisplayDragItemInCell] = useState(true);
+  const [myEvents, setMyEvents] = useState<Array<TCalendarEvent>>([]);
   const { id } = useAuth();
-  const [todosCalendar, setTodosCalendar] = useState<Array<TCalendarEvent>>()
-  
+  const [todosCalendar, setTodosCalendar] = useState<Array<TCalendarEvent>>();
+
   const {
     data: todoLists,
     isError,
@@ -54,8 +53,8 @@ export default function CalendarMod() {
 
   useEffect(() => {
     if (Array.isArray(todoLists)) {
-      todoLists.forEach(todoList => {
-        console.log(todoList.todo)
+      todoLists.forEach((todoList) => {
+        console.log(todoList.todo);
         if (Array.isArray(todoList.todo)) {
           const todosCalendar: Array<TCalendarEvent> = [];
           todoList.todo.forEach((todo: TTodo) => {
@@ -67,35 +66,55 @@ export default function CalendarMod() {
                 allDay: false,
                 start: new Date(todo.start_time),
                 end: new Date(todo.end_time),
-              }
-              todosCalendar.push(todoCalendar)
+              };
+              todosCalendar.push(todoCalendar);
               console.log(todo);
             } else {
               console.log("Not valid for calendar, rendering on todolist");
             }
           });
-          setTodosCalendar(todosCalendar)
+          setTodosCalendar(todosCalendar);
         } else {
           console.log("todoList.todoList is not an array");
         }
       });
     }
-  }, [todoLists, setTodosCalendar])
+  }, [todoLists, setTodosCalendar]);
 
-  const defaultDate = useMemo(() => new Date(2023, 9, 17), [])
+  const defaultDate = useMemo(() => new Date(2023, 9, 17), []);
 
+  /*
   const eventPropGetter = useCallback(
     (event: any) => ({
       ...(event.isDraggable
-        ? { className: 'isDraggable' }
-        : { className: 'nonDraggable' }),
+        ? { className: "isDraggable" }
+        : { className: "nonDraggable" }),
     }),
-    []
-  )
+    [],
+  );
+  */
 
-  const dragFromOutsideItem = useCallback(() => draggedEvent, [draggedEvent])
+  const eventPropGetter = () => {
+    var backgroundColor = "#039BE5";
+    var style = {
+      backgroundColor: backgroundColor,
+      borderRadius: "8px",
+      opacity: 1,
+      color: "white",
+      border: "1px solid #039BE5",
+      borderLeft: "3px solid #004F75",
+    };
+    return {
+      style: style,
+    };
+  };
 
-  const handleDragStart = useCallback((event: any) => setDraggedEvent(event), [])
+  const dragFromOutsideItem = useCallback(() => draggedEvent, [draggedEvent]);
+
+  const handleDragStart = useCallback(
+    (event: any) => setDraggedEvent(event),
+    [],
+  );
 
   const customOnDragOver = useCallback(
     (dragEvent: any) => {
@@ -104,105 +123,87 @@ export default function CalendarMod() {
       // onDragOver can optionally be passed to conditionally
       // allow draggable items to be dropped on cal, based on
       // whether event.preventDefault is called
-      if (draggedEvent !== 'undroppable') {
-        console.log('preventDefault')
-        dragEvent.preventDefault()
+      if (draggedEvent !== "undroppable") {
+        console.log("preventDefault");
+        dragEvent.preventDefault();
       }
     },
-    [draggedEvent]
-  )
+    [draggedEvent],
+  );
 
   const moveEvent = useCallback(
     ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }: any) => {
-      const { allDay } = event
+      const { allDay } = event;
       if (!allDay && droppedOnAllDaySlot) {
-        event.allDay = true
+        event.allDay = true;
       }
 
       setMyEvents((prev: any) => {
-        const existing = prev.find((ev: any) => ev.id === event.id) ?? {}
-        const filtered = prev.filter((ev: any) => ev.id !== event.id)
-        return [...filtered, { ...existing, start, end, allDay }]
-      })
+        const existing = prev.find((ev: any) => ev.id === event.id) ?? {};
+        const filtered = prev.filter((ev: any) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end, allDay }];
+      });
     },
-    [setMyEvents]
-  )
+    [setMyEvents],
+  );
 
   const newEvent = useCallback(
     (event: any) => {
       setMyEvents((prev) => {
-        const idList = prev.map((item) => item.id)
+        const idList = prev.map((item) => item.id);
         //const newId = Math.max(...idList) + 1
-        return [...prev, { ...event, id: "newId" }]
-      })
+        return [...prev, { ...event, id: "newId" }];
+      });
     },
-    [setMyEvents]
-  )
+    [setMyEvents],
+  );
 
   const onDropFromOutside = useCallback(
     ({ start, end, allDay: isAllDay }: any) => {
-      if (draggedEvent === 'undroppable') {
-        setDraggedEvent(null)
-        return
+      if (draggedEvent === "undroppable") {
+        setDraggedEvent(null);
+        return;
       }
 
-      const { title } = draggedEvent
+      const { title } = draggedEvent;
       const event = {
         title,
         start,
         end,
         isAllDay,
-      }
-      setDraggedEvent(null)
-      newEvent(event)
+      };
+      setDraggedEvent(null);
+      newEvent(event);
     },
-    [draggedEvent, setDraggedEvent, newEvent]
-  )
+    [draggedEvent, setDraggedEvent, newEvent],
+  );
 
   const resizeEvent = useCallback(
     ({ event, start, end }: any) => {
       setMyEvents((prev: any) => {
-        const existing = prev.find((ev: any) => ev.id === event.id) ?? {}
-        const filtered = prev.filter((ev: any) => ev.id !== event.id)
-        return [...filtered, { ...existing, start, end }]
-      })
+        const existing = prev.find((ev: any) => ev.id === event.id) ?? {};
+        const filtered = prev.filter((ev: any) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end }];
+      });
     },
-    [setMyEvents]
-  )
-
-  const items = [
-    {
-      name: "Arrastrame hacia el calendario",
-      val: 0
-    },
-    {
-      name: "Arrastrame hacia el calendario 2",
-      val: 0
-    }
-  ]
+    [setMyEvents],
+  );
 
   return (
-    <div style={{justifyContent: 'center', margin: 'auto'}}>
-      <div>
-        {Object.entries(items).map(([name, val]) => (
-          <div
-            draggable="true"
-            key={name}
-            onDragStart={() =>
-              handleDragStart({ title: val.name })
-            }
-          >
-            {val.name}
-          </div>
-        ))}
-      </div>
+    <div
+      style={{
+        justifyContent: "center",
+        margin: "auto",
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <DnDCalendar
         defaultDate={defaultDate}
         defaultView={Views.WEEK}
         localizer={localizer}
         events={todosCalendar}
-        style={{ height: '700px', width: '1000px'}}
-
+        style={{ height: "100%", width: "100%" }}
         eventPropGetter={eventPropGetter}
         onDragOver={customOnDragOver}
         onEventDrop={moveEvent}
@@ -215,4 +216,3 @@ export default function CalendarMod() {
     </div>
   );
 }
-
